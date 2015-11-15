1d1b0a93cebcafbe6c1abba73e30685b46d24d13
Entroper <entroper@gmail.com>
2015-11-15 02:49:06 -0500

    basic http server

---
+++ DotNode/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Http;
+
+namespace DotNode
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			HttpServer.CreateListener(context =>
+			{
+				context.Response.StatusCode = (int)HttpStatusCode.OK;
+				context.Response.ContentType = "text/plain";
+
+				using (var sw = new StreamWriter(context.Response.OutputStream))
+				{
+					sw.Write("Hello world!");
+				}
+			});
+
+			Console.ReadLine();
+		}
+	}
+}
---
+++ Http/HttpServer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Http
+{
+    public class HttpServer
+    {
+	    private readonly HttpListener _listener;
+
+	    public static HttpServer CreateListener(string prefix, Action<HttpListenerContext> callback)
+	    {
+		    var listener = new HttpServer(prefix);
+			listener.Listen(callback);
+
+		    return listener;
+	    }
+
+	    public static HttpServer CreateListener(Action<HttpListenerContext> callback)
+	    {
+			var listener = new HttpServer();
+			listener.Listen(callback);
+
+			return listener;
+		}
+
+		private HttpServer(string prefix = "http://*:80/")
+	    {
+		    _listener = new HttpListener();
+
+			_listener.Prefixes.Add(prefix);
+	    }
+
+	    private async void Listen(Action<HttpListenerContext> callback)
+	    {
+			_listener.Start();
+
+		    while (_listener.IsListening)
+		    {
+			    var context = await _listener.GetContextAsync();
+
+			    Task.Factory.StartNew(() => callback(context)).Forget();
+		    }
+	    }
+    }
+}
